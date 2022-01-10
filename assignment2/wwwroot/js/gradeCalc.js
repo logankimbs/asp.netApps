7d4192c5e0b17854f94b9ca6c72b1c66d9a0c005
logankimball <logankimbs@gmail.com>
2022-01-10 16:07:00 -0700

    added code

---
+++ assignment2/wwwroot/js/gradeCalc.js
@@ -0,0 +1,35 @@
+﻿$("#calcGrade").click(function () {
+    let assingments = $("#assignments").val() * 0.55;
+    let groupProjects = $("#groupProjects").val() * 0.05;
+    let quizzes = $("#quizzes").val() * 0.10;
+    let exams = $("#exams").val() * 0.20;
+    let intex = $("#intex").val() * 0.10;
+    let total = assingments + groupProjects + quizzes + exams + intex;
+    let results = Math.floor(total);
+
+    if (results >= 94) {
+        letterGrade = 'A';
+    } else if (results < 94 && results >= 90) {
+        letterGrade = 'A-';
+    } else if (results < 90 && results >= 87) {
+        letterGrade = 'B+';
+    } else if (results < 87 && results >= 84) {
+        letterGrade = 'B';
+    } else if (results < 84 && results >= 80) {
+        letterGrade = 'B-';
+    } else if (results < 80 && results >= 77) {
+        letterGrade = 'C+';
+    } else if (results < 77 && results >= 74) {
+        letterGrade = 'C';
+    } else if (results < 74 && results >= 70) {
+        letterGrade = 'C-';
+    } else if (results < 70 && results >= 67) {
+        letterGrade = 'D+';
+    } else if (results < 67 && results >= 64) {
+        letterGrade = 'D-';
+    } else {
+        letterGrade = 'E';
+    }
+
+    $("#demo").html(`Your grade is a ${results}% (${letterGrade})`);
+});
