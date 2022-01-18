78a3feff1d756ad61a7da2227714269526cb7591
logankimball <logankimbs@gmail.com>
2022-01-18 15:56:59 -0700

    converted to MVC to submit for mission 3 assignment

---
+++ assignment2/Controllers/Blah.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace assignment2.Controllers
+{
+    public class Blah : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ assignment2/Models/GradeCalc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment2.Models
+{
+    public class GradeCalcModel
+    {
+        [Range(0, 100, ErrorMessage = "Must be a number between 0 and 100")]
+        public int assignments { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Must be a number between 0 and 100")]
+        public int groupProjects { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Must be a number between 0 and 100")]
+        public int quizzes { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Must be a number between 0 and 100")]
+        public int exams { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Must be a number between 0 and 100")]
+        public int intex { get; set; }
+    }
+}
